41b6d08788225e3fad186340202474947f97df69
Guikai <guikai_huang@outlook.com>
2023-02-15 17:15:05 -0600

    CRUD tests

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -2,13 +2,15 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"testing"
+	"time"
 
 	"github.com/GK16/miniBank/util"
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreatAccount(t *testing.T){
+func creatRandomAccount(t *testing.T) Account {
 	arg := CreateAccountParams{
 		Owner: util.RandOwner(),
 		Balance: util.RandMoney(),
@@ -25,4 +27,71 @@ func TestCreatAccount(t *testing.T){
 
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
+
+	return account
+}
+
+func TestCreatAccount(t *testing.T){
+	creatRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T){
+	account1 := creatRandomAccount(t)
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, account1.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+}
+
+func TestUpdateAccount(t *testing.T){
+	account1 := creatRandomAccount(t)
+	arg := UpdateAccountParams{
+		ID: account1.ID,
+		Balance: util.RandMoney(),
+	}
+
+	account2, err := testQueries.UpdateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, arg.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+}
+
+func TestDeleteAccount(t *testing.T){
+	account1 := creatRandomAccount(t)
+	err := testQueries.DeleteAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, account2)
+}
+
+func TestListAccounts(t *testing.T){
+	for i:= 0; i < 10; i++ {
+		creatRandomAccount(t)
+	}
+
+	arg := ListAccountsParams{
+		Limit: 5, // skip the firt X records
+		Offset: 5, // return the next X records
+	}
+
+	accounts, err := testQueries.ListAccounts(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, accounts, 5)
+
+	for _, account := range accounts {
+		require.NotEmpty(t, account)
+	}
 }
